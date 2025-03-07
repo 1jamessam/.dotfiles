ac204eccba363e0bc7385db4df6c6c4b12733c27
1jamessam <tanapat.samranchit@gmail.com>
2025-03-07 14:44:15 +0100

    remove marksman config

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,15 +2,6 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
-      marksman = {
-        settings = {
-          marksman = {
-            diagnostics = {
-              disabled = { "MD013/line-length" },
-            },
-          },
-        },
-      },
       basedpyright = {
         settings = {
           basedpyright = {
