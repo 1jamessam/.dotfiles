57a70f8abd3f3800cb7b544ad7459966022a666c
James <tanapat.samranchit@gmail.com>
2026-01-08 15:42:51 +0700

    update

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -15,9 +15,9 @@ return {
       local dap_python = require("dap-python")
 
       require("dapui").setup()
-      require("nvim-dap-virtual-text").setup({
+      require("nvim-dap-virtual-text").setup {
         commented = true, -- Show virtual text alongside comment
-      })
+      }
 
       -- dap_python.setup(LazyVim.get_pkg_path("debugpy"))
       dap_python.setup("python")
@@ -44,50 +44,47 @@ return {
       })
 
       -- Automatically open/close DAP UI
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
+      dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
 
       local opts = { noremap = true, silent = true }
 
-      vim.keymap.set("n", "<leader>dm", function()
-        dap_python.test_method()
-      end, { desc = "Debug Test Method" })
+      vim.keymap.set(
+        "n",
+        "<leader>dm",
+        function() dap_python.test_method() end,
+        { desc = "Debug Test Method" }
+      )
 
       -- Toggle breakpoint
-      vim.keymap.set("n", "<leader>db", function()
-        dap.toggle_breakpoint()
-      end, { desc = "Toggle Breakpoint" })
+      vim.keymap.set(
+        "n",
+        "<leader>db",
+        function() dap.toggle_breakpoint() end,
+        { desc = "Toggle Breakpoint" }
+      )
 
       -- Continue / Start
-      vim.keymap.set("n", "<leader>dc", function()
-        dap.continue()
-      end, { desc = "Continue" })
+      vim.keymap.set("n", "<leader>dc", function() dap.continue() end, { desc = "Continue" })
 
       -- Step Over
-      vim.keymap.set("n", "<leader>do", function()
-        dap.step_over()
-      end, { desc = "Step Over" })
+      vim.keymap.set("n", "<leader>do", function() dap.step_over() end, { desc = "Step Over" })
 
       -- Step Into
-      vim.keymap.set("n", "<leader>di", function()
-        dap.step_into()
-      end, { desc = "Step Into" })
+      vim.keymap.set("n", "<leader>di", function() dap.step_into() end, { desc = "Step Into" })
 
       -- Step Out
-      vim.keymap.set("n", "<leader>dO", function()
-        dap.step_out()
-      end, { desc = "Step Out" })
+      vim.keymap.set("n", "<leader>dO", function() dap.step_out() end, { desc = "Step Out" })
 
       -- Keymap to terminate debugging
-      vim.keymap.set("n", "<leader>dq", function()
-        require("dap").terminate()
-      end, { desc = "Terminate" })
+      vim.keymap.set(
+        "n",
+        "<leader>dq",
+        function() require("dap").terminate() end,
+        { desc = "Terminate" }
+      )
 
       -- Toggle DAP UI
-      vim.keymap.set("n", "<leader>du", function()
-        dapui.toggle()
-      end, { desc = "Toggle" })
+      vim.keymap.set("n", "<leader>du", function() dapui.toggle() end, { desc = "Toggle" })
     end,
   },
 }
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -16,17 +16,6 @@ return {
         explorer = {
           hidden = true,
           ignored = true,
-          -- win = {
-          --   input = {
-          --     keys = {
-          --       ["<C-h>"] = { "<C-w>h", mode = { "i", "n" } },
-          --       -- ["<C-j>"] = { "<C-w>j", mode = { "i", "n" } }, -- This is the same as <C-Down>
-          --       ["<C-j>"] = { term_nav("j"), mode = { "i", "n" } }, -- This is the same as <C-Down>
-          --       ["<C-k>"] = { "<C-w>k", mode = { "i", "n" } },
-          --       ["<C-l>"] = { "<C-w>l", mode = { "i", "n" } },
-          --     },
-          --   },
-          -- },
         },
       },
     },
